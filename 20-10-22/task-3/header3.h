0c9017182a5e7127f0262b1cfcc3094a6ff2309c
Svetlana <90257465+ssolomvi@users.noreply.github.com>
2022-10-11 19:50:22 +0300

    Add files via upload

---
+++ 20-10-22/task-3/header3.h
@@ -0,0 +1,52 @@
+#pragma once
+#ifndef HEADER_3
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define euler 0.57721566490153286060
+
+/// <summary>
+/// Finds an aproximate number as a limit of an expression with given epsilon
+/// </summary>
+/// <param name="lim">- expression</param>
+/// <param name="eps">- given epsilon</param>
+/// <returns>a found limit / long double</returns>
+long double limit(long double lim(int n), double eps);
+long double e_lim(int n);
+long double pi_lim(int n);
+long double ln2_lim(int n);
+long double sqrt2_lim(double eps);
+long double euler_lim(double eps);
+
+/// <summary>
+/// Finds an aproximate number as a sum of "function" returned values with given epsilon
+/// </summary>
+/// <param name="function">- function which returns values</param>
+/// <param name="eps">- given epsilon</param>
+/// <param name="start">- value to start summing from</param>
+/// <returns>found aproximate number</returns>
+long double sum(long double function(int n), double eps, int start);
+
+/// <summary>
+/// Finds an aproximate number as a multiple of "function" returned values with given epsilon
+/// </summary>
+/// <param name="function">- function which returns values</param>
+/// <param name="eps">- given epsilon</param>
+/// <param name="start">- value to start multipling from</param>
+/// <returns>found aproximate number</returns>
+long double mult(long double function(int n), double eps, int start);
+long double e_row(int n);
+long double pi_row(int n);
+long double ln2_row(int n);
+long double sqrt2_mult(int k);
+long double gamma_row(int k);
+
+double equation_dichotomy(double equation(double x), double a, double b, double eps);
+double e_equation(double x);
+double pi_equation(double x);
+double ln2_equation(double x);
+double sqrt2_equation(double x);
+double euler_equation(double eps);
+
+#endif // HEADER_3
---
+++ 20-10-22/task-3/main3.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "header3.h"
+
+
+int main(void)
+{
+	int l;
+	printf("Enter precision:\n");
+	scanf("%d", &l);
+ 	double eps = pow(10, -l);
+	l++; // as using %.*lf rounds the number (l = 2: 0.578 -> 0.58)
+	
+	printf("e constant:\n");
+	printf("\tas limit value: %.*Lf\n", l, limit(e_lim, eps));
+	printf("\tas row sum: %.*Lf\n", l, sum(e_row, eps, 0));
+	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(e_equation, 2., 3., eps));
+
+	printf("pi constant:\n");
+	printf("\tas limit value: %.*Lf\n", l, limit(pi_lim, eps));
+	printf("\tas row sum: %.*Lf\n", l, sum(pi_row, eps, 1));
+	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(pi_equation, 3, 3.5, eps));
+	
+	printf("ln(2) constant:\n");
+	printf("\tas limit value: %.*lf\n", l, limit(ln2_lim, eps));
+	printf("\tas row sum: %.*lf\n", l, sum(ln2_row, eps, 1));
+	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(ln2_equation, 0, 1, eps));
+
+	printf("sqrt(2) constant:\n");
+	printf("\tas limit value: %.*lf\n", l, sqrt2_lim(eps));
+	printf("\tas row product: %.*lf\n", l, mult(sqrt2_mult, eps, 2));
+	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(sqrt2_equation, 1, 2, eps));
+
+	printf("Euler's constant:\n");
+	printf("\tas limit value: %.*lf\n", l, euler_lim(eps));
+	double constant_pi = M_PI * M_PI / 6;
+	printf("\tas row sum: %.*lf\n", l, sum(gamma_row, eps, 2) - constant_pi);
+	printf("\tas solution of an equation: %.*lf\n\n", l, euler_equation(eps));
+
+	return 0;
+}
