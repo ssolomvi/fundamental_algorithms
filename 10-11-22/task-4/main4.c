4fcc7f7c93d8e414d1f04a86171347a84e61d0ae
ssolomvi <ssolomvi@gmail.com>
2022-11-08 21:39:32 +0300

    merge fix

--- 10-11-22/task-4/main4.c
+++ 10-11-22/task-4/main4.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "header4.h"
 
 int main(int argc, char** argv)
@@ -92,99 +91,6 @@ int main(int argc, char** argv)
 	case read_from_csv_file_realloc_error:
 		printf("Memory reallocation error happened\n");
 		return -13;
-=======
-#include "header4.h"
-
-int main(int argc, char** argv)
-{
-	if (argc != 2) {
-		printf("Incorrect count of command line arguments.\nPass the stop lexeme as command line argument\n");
-		return -1;
-	}
-
-	int tries = 3;
-	char user_stream[BUFSIZ], ** messages = NULL, * filename_out = gen_file_name();
-	FILE* out = NULL, * in = NULL;
-	if (!(out = fopen(filename_out, "w"))) {
-		printf("Error opening output scv file\n");
-		return -2;
-	}
-	read_n_write_csv_statuses read_n_write_csv_s = -1;
-
-	while (tries) {
-		printf("Enter the source from where to read messages:\n>>> ");
-
-		if ((scanf("%s", user_stream)) != 1) {
-			printf("Error reading a string!\n");
-			return -3;
-		}
-		if (!strcmp(user_stream, "stdin")) {
-			read_n_write_csv_s = read_n_write_csv(stdin, out, argv[1]);
-		}
-		else {
-			if (!(in = fopen(user_stream, "r"))) {
-				tries--;
-				printf("Incorrect file name passed / error opening file %s. You have %d tries left\n\n", user_stream, tries);
-			}
-			read_n_write_csv_s = read_n_write_csv(in, out, argv[1]);
-		}
-
-		if (read_n_write_csv_s != -1) {
-			switch (read_n_write_csv_s)
-			{
-			case read_n_write_malloc_error:
-				printf("Memory allocation error happened\n");
-				return -4;
-			case read_n_write_realloc_error:
-				printf("Memory reallocation error happened\n");
-				return -5;
-			case read_n_write_file_ptr_error:
-				printf("Incorrect ptr to file passed in function read_n_write_csv\n");
-				return -6;
-			case read_n_write_incorrect_stop_lexeme:
-				printf("Incorrect ptr to stop lexeme / incorrect stop lexeme passed in function read_n_write_csv\n");
-				return -7;
-			case read_n_write_fgets_error:
-				printf("Error happened with fgets function in function read_n_write_csv\n");
-				return -8;
-			case read_n_write_message_error:
-				printf("Incorrect message passed in function in_csv_file\n");
-				return -9;
-			default:
-				printf("All the data was successfully read from input stream!\nIt's wrote down in file %s\n\n", filename_out);
-				break;
-			}
-			break;
-		}
-	}
-
-	fclose(in);
-	fclose(out);
-
-	if (!(out = fopen(filename_out, "r"))) {
-		printf("Error opening file\n");
-		return -2;
-	}
-	
-	message* arr = NULL;
-	int arr_count = 0;
-	read_from_csv_file_statuses read_from_csv_file_s = read_from_csv_file(out, &arr, &arr_count);
-
-	switch (read_from_csv_file_s)
-	{
-	case read_from_csv_file_incorrect_ptr_to_file:
-		printf("Incorrect ptr to file passed in function read_from_csv_file\n");
-		return -10;
-	case read_from_csv_file_incorrect_ptr_to_messages_arr:
-		printf("Incorrect ptr to messages array passed in function read_from_csv_file (it must be NULL)\n");
-		return -11;
-	case read_from_csv_file_malloc_error:
-		printf("Memory allocation error happened\n");
-		return -12;
-	case read_from_csv_file_realloc_error:
-		printf("Memory reallocation error happened\n");
-		return -13;
->>>>>>> origin/main
 	default:
 
 		while (tries) {
@@ -212,18 +118,9 @@ int main(int argc, char** argv)
 				break;
 			}
 		}
-		
-<<<<<<< HEAD
 		break;
 	}
 	fclose(out);
 	free(filename_out);
 	return 0;
-=======
-		break;
-	}
-	fclose(out);
-	free(filename_out);
-	return 0;
->>>>>>> origin/main
 }
--- 10-11-22/task-7/header7.h
+++ 10-11-22/task-7/header7.h
@@ -1,5 +1,7 @@
-<<<<<<< HEAD
 #ifndef HEADER_7
+
+#define HEADER_7
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -46,52 +48,3 @@ read_citizens_statuses read_citizens(FILE* input, list_cit** head);
 void print_list(FILE* stream, list_cit* to_print);
 
 #endif // !HEADER_7
-=======
-#ifndef HEADER_7
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-typedef enum sex {
-	male = 1,
-	female = 2
-} sex;
-
-typedef struct date {
-	unsigned year;
-	unsigned month;
-	unsigned day;
-} date;
-
-typedef struct citizen {
-	char* surname;
-	char* name;
-	char* patronymic;
-	date birthday;
-	sex sex;
-	float average_income;
-} citizen;
-
-typedef struct list_cit {
-	citizen* data;
-	struct list_cit* next;
-} list_cit;
-
-typedef enum read_citizens_statuses {
-	read_citizens_incorrect_ptr_to_file,
-	read_citizens_incorrect_ptr_to_list,
-	read_citizens_incorrect_ptr_to_value,
-	read_citizens_incorrect_data,
-	read_citizens_malloc_error,
-	read_citizens_realloc_error,
-	read_citizens_ok
-} read_citizens_statuses;
-
-void delete_citizens(list_cit* to_del);
-read_citizens_statuses read_citizens(FILE* input, list_cit* head);
-void print_list(FILE* stream, list_cit* to_print);
-
-#endif // !HEADER_7
->>>>>>> origin/main
