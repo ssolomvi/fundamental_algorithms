8582c7ab58006fb449b4971c629f47121246b71c
ssolomvi <ssolomvi@gmail.com>
2022-11-10 02:40:40 +0300

    lab3task8: reading in binary tree done (not debugged yet)

--- lab3/lab3task8/header8.h
+++ lab3/lab3task8/header8.h
@@ -1,8 +1,60 @@
 #ifndef HEADER_8
+#define HEADER_8
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct binary_tree_node {
+	char* lexeme;
+	size_t count_occured;
+	struct tree_node* left;
+	struct tree_node* right;
+} binary_tree_node;
+
+typedef struct binary_tree {
+	binary_tree_node* root;
+} binary_tree;
+
+typedef struct prefix_tree_node {
+	char ch;                                 // key
+	char allocated;							 // count of allocated elements in child_array
+	struct prefix_tree_node** child_array;	 // array of child trees
+	binary_tree_node* ptr_to_bin_node;		 // ptr to node in binary tree. NULL if no such lexeme
+} prefix_tree_node;
+
+typedef struct prefix_tree {
+	prefix_tree_node* root;
+} prefix_tree;
+
+typedef enum prefix_tree_statuses {
+	pref_malloc_error,
+	pref_realloc_error,
+	pref_ok,
+} prefix_tree_statuses;
+
+typedef enum binary_tree_statuses {
+	bin_malloc_error,
+	bin_ok,
+} binary_tree_statuses;
+
+typedef enum read_in_binary_tree_statuses {
+	read_in_binary_tree_incorrect_ptr_to_file,
+	read_in_binary_tree_incorrect_ptr_to_tree,
+	read_in_binary_tree_malloc_error,
+	read_in_binary_tree_realloc_error,
+	read_in_binary_tree_ok,
+} read_in_binary_tree_statuses;
+
+typedef enum read_lexeme_statuses {
+	read_lexeme_eof_reached,
+	read_lexeme_malloc_error,
+	read_lexeme_realloc_error,
+	read_lexeme_ok
+} read_lexeme_statuses;
+
+void delete_binary_tree(binary_tree_node** bin);
+void delete_prefix_tree(prefix_tree_node** pref);
 
 #endif // !HEADER_8
