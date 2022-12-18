b9c6ce560a1c5a0d58ea35fdf85b131e0b5800a8
ssolomvi <ssolomvi@gmail.com>
2022-12-18 16:01:19 +0300

    3.8 done, 3.9, 3.10 done

--- lab3/lab3task8/header8.h
+++ lab3/lab3task8/header8.h
@@ -73,13 +73,6 @@ void delete_prefix_tree(prefix_tree_node** pref);
 /// <returns>read_in_binary_tree_statuses</returns>
 read_in_binary_tree_statuses read_in_binary_tree(FILE* stream, binary_tree* my_bin_tree);
 
-typedef enum find_min_max_statuses {
-	find_min_max_tree_is_empty,
-	find_min_max_ok
-} find_min_max_statuses;
-
-find_min_max_statuses find_min_max(binary_tree tree);
-
 void bin_tree_detour_in_depth(binary_tree_node* node, void* structure, int traverse(binary_tree_node*, void*));
 
 typedef enum put_in_file_statuses {
@@ -96,5 +89,42 @@ put_in_file_statuses put_in_file(FILE* stream, binary_tree my_tree);
 /// <param name="stream">- the output stream</param>
 /// <param name="tree">- binary tree to print</param>
 void print_bin_tree(FILE* stream, binary_tree tree);
+
+
+typedef enum find_times_met_statuses {
+	find_times_met_no_such_word,
+	find_times_met_ok
+} find_times_met_statuses;
+
+typedef struct times_met_str {
+	char* to_find;
+	size_t count_occured;
+} times_met_str;
+
+find_times_met_statuses find_times_met(binary_tree* tree, char* to_find, times_met_str* structure);
+
+typedef enum find_n_most_often_statuses {
+	find_n_most_often_tree_is_empty,
+	find_n_most_often_incorrect_n_to_find,
+	find_n_most_often_malloc_error,
+	find_n_most_often_count_of_elements_is_less,
+	find_n_most_often_incorrect_ptr_to_lexeme,
+	find_n_most_often_ok
+} find_n_most_often_statuses;
+
+find_n_most_often_statuses find_n_often(binary_tree tree, char*** lexemes, unsigned int* processed, unsigned int n);
+
+typedef enum find_min_max_statuses {
+	find_min_max_tree_is_empty,
+	find_min_max_ok
+} find_min_max_statuses;
+
+typedef struct find_min_max_str {
+	char* max;
+	char* min;
+} find_min_max_str;
+
+find_min_max_statuses find_min_max(binary_tree tree, find_min_max_str* structure);
+
 
 #endif // !HEADER_8
--- lab3/lab3task8/main8.c
+++ lab3/lab3task8/main8.c
@@ -74,9 +74,65 @@ int main(int argc, char** argv)
 		}
 	}
 
-	printf("\n\nBinary tree after putting in file out.txt:\n");
+	printf("\n\nBinary tree after putting in file out.txt:\n\n");
 	print_bin_tree(stdout, bin_tree);
 #pragma endregion
+
+	printf("Enter word to check how many times it occured in file %s:\n>>> ", argv[1]);
+	char user_input[BUFSIZ];
+	scanf("%s", user_input);
+	times_met_str times_met_struct;
+	find_times_met_statuses find_times_met_s = find_times_met(&bin_tree, user_input, &times_met_struct);
+	if (find_times_met_s == find_times_met_no_such_word) {
+		printf("No %s in file %s found!\n\n", user_input, argv[1]);
+	}
+	else {
+		printf("Lexeme %s occured in file %s %Iu times\n\n", user_input, argv[1], times_met_struct.count_occured);
+	}
+
+	
+	unsigned int find_n_most = 0, iter_most_often = 0, processed = 0;
+	char** n_most_often_lexemes = NULL;
+	printf("Enter wished count of words which are most often met in file %s:\n>>> ", argv[1]);
+	scanf("%d", &find_n_most);
+	find_n_most_often_statuses find_n_most_often_s = find_n_often(bin_tree, &n_most_often_lexemes,
+		&processed, find_n_most);
+	switch (find_n_most_often_s)
+	{
+	case find_n_most_often_incorrect_n_to_find:
+		printf("n must not be <= 0!\n");
+		break;
+	case find_n_most_often_malloc_error:
+		printf("Memory allocation error happened!\n");
+		delete_binary_tree(&(bin_tree.root));
+		return -11;
+	case find_n_most_often_count_of_elements_is_less:
+		printf("Count of elements in binary tree (%u) is less then your n = %u\n\n", processed, find_n_most);
+		break;
+	case find_n_most_often_incorrect_ptr_to_lexeme:
+		printf("Incorrect ptr to lexemes array passed!\n");
+		break;
+	default:
+		printf("%d most often met lexemes:\n", find_n_most);
+		for (iter_most_often = 0; iter_most_often < find_n_most; iter_most_often++) {
+			printf("%s ", n_most_often_lexemes[iter_most_often]);
+		}
+		printf("\n\n");
+		free(n_most_often_lexemes);
+		break;
+	}
+	
+
+	// ok omg my string wizard is is is is
+	find_min_max_str min_max;
+	find_min_max_statuses find_min_max_s = find_min_max(bin_tree, &min_max);
+	if (find_min_max_s == find_min_max_tree_is_empty) {
+		printf("Your tree is empty! So it's not possible to find min and max lexemes!\n");
+	}
+	else {
+		printf("Max lexeme in your binary tree: %s\nMin lexeme in your binary tree: %s\n\n", 
+			min_max.max, min_max.min);
+	}
 
 	delete_binary_tree(&(bin_tree.root));
 	return 0;
