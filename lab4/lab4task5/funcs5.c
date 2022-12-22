9fd837c05d20763315681e5d5964e499cc8885b2
ssolomvi <ssolomvi@gmail.com>
2022-12-22 08:25:09 +0300

    half-through 4.5

---
+++ lab4/lab4task5/funcs5.c
@@ -0,0 +1,401 @@
+﻿#include "header5.h"
+
+int op_prior(const char c)
+{
+    switch (c)
+    {
+    case '^':
+        return 3;
+
+    case '*':
+    case '/':
+    case '%':
+        return 2;
+
+    case '+':
+    case '-':
+        return 1;
+    }
+    return 0;
+}
+
+bool op_left_assoc(const char c)
+{
+    switch (c)
+    {
+    case '*':
+    case '/':
+    case '%':
+    case '+':
+    case '-':
+    case '^':
+        return true;
+    }
+    return false;
+}
+
+unsigned int op_arg_count(const char c)
+{
+    switch (c)
+    {
+    case '*':
+    case '/':
+    case '%':
+    case '+':
+    case '-':
+    case '^':
+        return 2;
+
+    default:
+        return c - 'A';
+    }
+    return 0;
+}
+
+funcs_responses push_stack(stack_struct* stack, char ch)
+{
+    lin_list_elem* new_elem = (lin_list_elem*)malloc(sizeof(lin_list_elem));
+
+    if (!new_elem)
+        return funcs_realloc_error;
+
+    new_elem->data = ch;
+
+    if (!(stack->count_of_elements)) {
+        stack->top = new_elem;
+        stack->top->next = NULL;
+        stack->count_of_elements = 1;
+    }
+    else {
+        new_elem->next = stack->top;
+        stack->top = new_elem->next;
+        stack->count_of_elements++;
+    }
+
+    return funcs_ok;
+}
+
+void pop_stack(stack_struct* stack)
+{
+    if (!(stack->count_of_elements))
+        return;
+
+    lin_list_elem* tmp = stack->top;
+    stack->top = stack->top->next;
+    free(tmp);
+    stack->count_of_elements--;
+}
+
+void delete_stack(stack_struct* stack)
+{
+    while (stack->count_of_elements) {
+        pop_stack(&stack);
+    }
+}
+
+funcs_responses shunting_yard(const char* input, char** output)
+{
+    if ((*output))
+        return funcs_incorrect_ptr_to_str;
+
+    char* input_cur_pos = input, * input_end = input + strlen(input);
+    unsigned allocated_out = 16, act_size_out = 0;
+
+    if (!((*output) = (char*)malloc(sizeof(char) * allocated_out))) {
+        return funcs_malloc_error;
+    }
+
+    char curr_ch = 0, prev_ch = 0, stack_top_op = 0, * tmp = NULL;
+    bool left_bracket_appeared = false;
+
+    stack_struct stack;
+    stack.top = NULL; stack.count_of_elements = 0;
+    funcs_responses funcs_re = -1;
+
+    while (input_cur_pos < input_end) {
+        curr_ch = *input_cur_pos;
+
+        if (curr_ch != ' ') {
+            if (is_ident(curr_ch)) {
+                if (allocated_out <= act_size_out) {
+                    allocated_out *= 2;
+                    if (!(tmp = (char*)realloc((*output), sizeof(char) * allocated_out))) {
+                        free(*output);
+                        return funcs_realloc_error;
+                    }
+                    (*output) = tmp;
+                }
+                (*output)[act_size_out++] = curr_ch;
+            }
+
+            else if (is_operator(curr_ch)) {
+                while (stack.count_of_elements > 0) {
+                    stack_top_op = stack.top->data;
+
+                    if (is_operator(stack_top_op) &&
+                        ((op_left_assoc(curr_ch) && (op_prior(curr_ch) <= op_prior(stack_top_op))) ||
+                            (!op_left_assoc(curr_ch) && (op_prior(curr_ch) < op_prior(stack_top_op))))) {
+                        if (allocated_out <= act_size_out) {
+                            allocated_out *= 2;
+                            if (!(tmp = (char*)realloc((*output), sizeof(char) * allocated_out))) {
+                                free(*output);
+                                delete_stack(&stack);
+                                return funcs_realloc_error;
+                            }
+                            (*output) = tmp;
+                        }
+                        (*output)[act_size_out++] = curr_ch;
+                        pop_stack(&stack);
+                    }
+                    else {
+                        break;
+                    }
+                }
+                if ((funcs_re = push_stack(&stack, curr_ch)) != funcs_ok) {
+                    free(*output);
+                    delete_stack(&stack);
+                    return funcs_re;
+                }
+            }
+
+            else if (curr_ch == '(') {
+                if ((funcs_re = push_stack(&stack, curr_ch)) != funcs_ok) {
+                    free(*output);
+                    delete_stack(&stack);
+                    return funcs_re;
+                }
+            }
+
+            else if (curr_ch == ')') {
+                left_bracket_appeared = false;
+
+                while (stack.count_of_elements > 0) {
+                    stack_top_op = stack.top->data;
+
+                    if (stack_top_op == '(') {
+                        left_bracket_appeared = true;
+                        pop_stack(&stack);
+                        break;
+                    }
+                    else {
+                        if (allocated_out <= act_size_out) {
+                            allocated_out *= 2;
+                            if (!(tmp = (char*)realloc((*output), sizeof(char) * allocated_out))) {
+                                free(*output);
+                                delete_stack(&stack);
+                                return funcs_realloc_error;
+                            }
+                            (*output) = tmp;
+                        }
+                        (*output)[act_size_out++] = curr_ch;
+
+                        pop_stack(&stack);
+                    }
+                }
+
+                if (!left_bracket_appeared) {
+                    free(*output);
+                    delete_stack(&stack);
+                    return expr_parentheses_mismatched;
+                }
+            }
+
+            else {
+                return expr_unknown_token;
+            }
+        }
+        if (is_ident(prev_ch) && curr_ch == ' ') {
+            if (allocated_out <= act_size_out) {
+                allocated_out *= 2;
+                if (!(tmp = (char*)realloc((*output), sizeof(char) * allocated_out))) {
+                    free(*output);
+                    delete_stack(&stack);
+                    return funcs_realloc_error;
+                }
+                (*output) = tmp;
+            }
+            (*output)[act_size_out++] = curr_ch;
+        }
+
+        prev_ch = curr_ch;
+        ++input_cur_pos;
+    }
+
+    while (stack.count_of_elements > 0) {
+        stack_top_op = stack.top->data;
+
+        if (stack_top_op == '(' || stack_top_op == ')') {
+            free(*output);
+            delete_stack(&stack);
+            return expr_parentheses_mismatched;
+        }
+
+        if (allocated_out <= act_size_out) {
+            allocated_out *= 2;
+            if (!(tmp = (char*)realloc((*output), sizeof(char) * allocated_out))) {
+                free(*output);
+                delete_stack(&stack);
+                return funcs_realloc_error;
+            }
+            (*output) = tmp;
+        }
+        (*output)[act_size_out++] = curr_ch;
+        pop_stack(&stack);
+    }
+
+    if (!(tmp = (char*)realloc((*output), sizeof(char) * (act_size_out + 1)))) {
+        free(*output);
+        delete_stack(&stack);
+        return funcs_realloc_error;
+    }
+    (*output) = tmp;
+    (*output)[act_size_out] = 0;
+
+    return funcs_ok;
+}
+
+funcs_responses push_in_double_stack(stack_struct_double* stack, double data)
+{
+    lin_list_elem_double* new_elem = (lin_list_elem_double*)malloc(sizeof(lin_list_elem_double));
+    if (!new_elem)
+        return funcs_malloc_error;
+    new_elem->data = data;
+
+    if (!(stack->top)) {
+        new_elem->next = NULL;
+        stack->top = new_elem;
+        stack->count_of_elements = 1;
+    }
+    else {
+        new_elem->next = stack->top;
+        stack->top = new_elem;
+        stack->count_of_elements++;
+    }
+    return funcs_ok;
+}
+
+double pop_from_double_stack(stack_struct_double* stack)
+{
+    if (!stack->top)
+        return 0;
+
+    double to_return = stack->top->data;
+    lin_list_elem_double* tmp = stack->top;
+    stack->top = stack->top->next;
+    free(tmp);
+    stack->count_of_elements--;
+    return to_return;
+}
+
+void delete_double_stack(stack_struct_double* stack)
+{
+    if (!stack->top)
+        return 0;
+
+    lin_list_elem_double* ptr = stack->top, * tmp = NULL;
+    while (ptr) {
+        tmp = ptr;
+        ptr = ptr->next;
+        free(tmp);
+    }
+    stack->top = NULL;
+    stack->count_of_elements = 0;
+}
+
+double to_num(char ch)
+{
+    return ((ch >= '0' && ch <= '9') ? ch - '0' : ch);
+}
+
+double perform_operation(char op, double fi, double se)
+{
+    switch (op)
+    {
+    case '*':
+        return fi * se;
+    case '/':
+        return fi / se;
+    case '%':
+        return ((long)fi) % ((long)se);
+    case '+':
+        return fi + se;
+    case '-':
+        return fi - se;
+    case '^':
+        return pow(fi, se);
+    }
+    return 0;
+}
+
+funcs_responses calculate_postfix_notation(char* str, double* res)
+{
+    (*res) = 0;
+    size_t i, str_len = strlen(str);
+    stack_struct_double stack;
+    stack.top = NULL;
+    funcs_responses funcs_s = -1;
+    char ch = 0, prev_ch = 0;
+    double number = 0, mult = 1, fi = 0, se = 0;
+    bool met_comma = false;
+    unsigned count_of_args = 0;
+
+    for (i = 0; i < str_len; i++) {
+        ch = str[i];
+        if (is_ident(ch)) {
+            if (prev_ch == 0 || is_ident(prev_ch)) {
+                if (!met_comma) {
+                    number *= 10;
+                    number += to_num(ch);
+                }
+            }
+            else if (prev_ch == ',' || prev_ch == '.') {
+                met_comma = true;
+            }
+            if (met_comma) {
+                mult /= 10;
+                number += to_num(ch) * mult;
+            }
+        }
+        else {
+            if (is_ident(prev_ch)) {
+                funcs_s = push_in_double_stack(&stack, number);
+                if (funcs_s == funcs_malloc_error) {
+                    delete_double_stack(&stack);
+                    return funcs_s;
+                }
+                number = 0;
+                mult = 1;
+            }
+            if (is_operator(ch)) {
+                count_of_args = op_arg_count(ch);
+                if (stack.count_of_elements < count_of_args) {
+                    delete_double_stack(&stack);
+                    return expr_insufficient_count_of_arguments;
+                }
+
+                if (ch == '/' && stack.top->data == 0) {
+                    delete_double_stack(&stack);
+                    return expr_division_by_zero;
+                }
+
+                se = pop_from_double_stack(&stack);
+                fi = pop_from_double_stack(&stack);
+                funcs_s = push_in_double_stack(&stack,
+                    perform_operation(ch, fi, se));
+                if (funcs_s == funcs_malloc_error) {
+                    delete_double_stack(&stack);
+                    return funcs_s;
+                }
+            }
+        }
+        
+        prev_ch = ch;
+    }
+    if (!stack.top) {
+        delete_double_stack(&stack);
+        return funcs_totally_incorrect_expr;
+    }
+    (*res) = stack.top->data;
+    delete_double_stack(&stack);
+    return funcs_ok;
+}
---
+++ lab4/lab4task5/main5.c
@@ -0,0 +1,233 @@
+#include "header5.h"
+#define size_of_input 256
+
+void delete_output_struct_arr(output_struct** arr, unsigned size)
+{
+	unsigned i;
+	for (i = 0; i < size; i++) {
+		free((*arr)[i].input);
+		free((*arr)[i].output);
+	}
+	free(*arr);
+	(*arr) = NULL;
+}
+
+int handle_responses(funcs_responses funcs_s) {
+	switch (funcs_s)
+	{
+	case funcs_incorrect_ptr_to_str:
+		printf("Incorrect ptr to output str passed. It must be NULL\n");
+		return -3;
+	case funcs_malloc_error:
+		printf("Memory allocation error happened\n");
+		return -4;
+	case funcs_realloc_error:
+		printf("Memory reallocation error happened\n");
+		return -5;
+	case funcs_totally_incorrect_expr:
+		printf("Totally incorrect expression detected\n");
+		return -6;
+	default:
+		break;
+	}
+	return 0;
+}
+
+funcs_responses make_error_filename(char* initial, char** error)
+{
+	size_t str_len = strlen(initial), i;
+	if (!((*error) = (char*)malloc(sizeof(char) * (str_len + 7)))) {
+		return funcs_malloc_error;
+	}
+
+	for (i = 0; i < str_len; i++) {
+		if (initial[i] == '.')
+			break;
+	}
+
+	char* tmp = (*error) + i;
+	strcpy((*error), initial);
+	strcpy(tmp, "_error");
+	strcpy(tmp + 6, initial + i);
+	return funcs_ok;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2) {
+		printf("Incorrect count of command line arguments.\nPass text file(s) with correct arithmetic expressions\n");
+		return -1;
+	}
+	/*
+	1. open file
+	2. read all strings of it in array of special struct
+	3. start making output
+	4. calculate one
+	5. print to stdout
+	6. close file
+	*/
+
+	FILE* in = NULL, * error_out = NULL;
+	output_struct* output = NULL, * tmp = NULL;
+	funcs_responses funcs_s = 0;
+
+	unsigned i, j, allocated_out = 8, act_size = 0;
+	int handle = 0;
+	size_t file_str_length = 0;
+	double res_calculus = 0;
+
+	char* filename_error = NULL;
+
+	for (i = 0; i < argc - 1; i++) {
+		allocated_out = 8; act_size = 0;
+		if (!(in = fopen(argv[i], "r"))) {
+			printf("Open file error\n");
+			return -2;
+		}
+
+		if (!(output = (output_struct*)malloc(sizeof(output_struct) * allocated_out))) {
+			fclose(in);
+			handle = handle_responses(funcs_malloc_error);
+			return handle;
+		}
+
+		// read all strings of it in array of special struct
+		while (!feof(in)) {
+			if (!(output[act_size].input = (char*)malloc(sizeof(char) * size_of_input))) {
+				fclose(in);
+				delete_output_struct_arr(&output, act_size);
+				handle = handle_responses(funcs_malloc_error);
+				return handle;
+			}
+			fgets(output[act_size].input, size_of_input, in);
+			file_str_length = strlen(output[act_size].input);
+			if (output[act_size].input[file_str_length - 1] == '\n')
+				output[act_size].input[file_str_length - 1] = '\0';
+			
+			if (output[act_size].input[0] == 0) {
+				if (!(output[act_size].output = (char*)malloc(sizeof(char) * 20))) {
+					fclose(in);
+					delete_output_struct_arr(&output, act_size);
+					free(output[act_size].input);
+					handle = handle_responses(funcs_malloc_error);
+					return handle;
+				}
+				strcpy(output[act_size].output, "Empty string passed");
+				output[act_size].output[19] = '\0';
+				output[act_size].result = -1;
+			}
+			else {
+				// start making output string
+				output[act_size].output = NULL;
+				funcs_s = shunting_yard(output[act_size].input, &(output[act_size].output));
+				if ((handle = handle_responses(funcs_s)) != 0) {
+					fclose(in);
+					delete_output_struct_arr(&output, act_size);
+					free(output[act_size].input);
+					return handle;
+				}
+			}
+			
+			if (funcs_s == expr_parentheses_mismatched) {
+				if (!(output[act_size].output = (char*)malloc(sizeof(char) * 23))) {
+					fclose(in);
+					delete_output_struct_arr(&output, act_size);
+					free(output[act_size].input); 
+					handle = handle_responses(funcs_malloc_error);
+					return handle;
+				}
+				strcpy(output[act_size].output, "Parentheses mismatched");
+				output[act_size].output[22] = '\0';
+				output[act_size].result = -1;
+			}
+			else if (funcs_s == expr_unknown_token) {
+				if (!(output[act_size].output = (char*)malloc(sizeof(char) * 20))) {
+					fclose(in);
+					delete_output_struct_arr(&output, act_size);
+					free(output[act_size].input);
+					handle = handle_responses(funcs_malloc_error);
+					return handle;
+				}
+				strcpy(output[act_size].output, "Unknown token found");
+				output[act_size].output[19] = '\0';
+				output[act_size].result = -1;
+			}
+			
+
+
+			// calculate one
+			// TODO delete output if expr 
+			funcs_s = calculate_postfix_notation(output[act_size].output, &res_calculus);
+			if ((handle = handle_responses(funcs_s)) != 0) {
+				fclose(in);
+				delete_output_struct_arr(&output, act_size);
+				free(output[act_size].input);
+				return handle;
+			}
+
+			if (funcs_s == expr_division_by_zero) {
+				free(output[act_size].output);
+				if (!(output[act_size].output = (char*)malloc(sizeof(char) * 17))) {
+					fclose(in);
+					delete_output_struct_arr(&output, act_size);
+					free(output[act_size].input);
+					handle = handle_responses(funcs_malloc_error);
+					return handle;
+				}
+				strcpy(output[act_size].output, "Division by zero");
+				output[act_size].output[16] = '\0';
+				output[act_size].result = -1;
+			}
+			else if (funcs_s == expr_insufficient_count_of_arguments) {
+				free(output[act_size].output);
+				if (!(output[act_size].output = (char*)malloc(sizeof(char) * 47))) {
+					fclose(in);
+					delete_output_struct_arr(&output, act_size);
+					free(output[act_size].input);
+					handle = handle_responses(funcs_malloc_error);
+					return handle;
+				}
+				strcpy(output[act_size].output, "Insufficient number of arguments in expression");
+				output[act_size].output[46] = '\0';
+				output[act_size].result = -1;
+			}
+			
+		}
+		// print to stdout / file
+		funcs_s = make_error_filename(argv[i], &(filename_error));
+		if (funcs_s == funcs_malloc_error) {
+			fclose(in);
+			handle = handle_responses(funcs_malloc_error);
+			return handle;
+		}
+
+		if (!(error_out = fopen(filename_error, "w"))) {
+			printf("Open file error\n");
+			return -2;
+		}
+
+		printf("===== Below are results for file %s =====", argv[i]);
+
+		for (j = 0; j < act_size; j++) {
+			if (!strcmp("Parentheses mismatched", output[j].output) ||
+				!strcmp("Unknown token found", output[j].output) ||
+				!strcmp("Division by zero", output[j].output) ||
+				!strcmp("Empty string passed", output[j].output) ||
+				!strcmp("Insufficient number of arguments in expression", output[j].output)) {
+				printf("[] %s\n   %s\n", output[j].input, output[j].output);
+			}
+			else {
+				printf("[] %s\n   %s\n   %lf\n", output[j].input, output[j].output, output[j].result);
+			}
+		}
+
+
+		delete_output_struct_arr(&output, act_size);
+		free(filename_error);
+		fclose(error_out);
+		fclose(in);
+	}
+
+
+	return 0;
+}
